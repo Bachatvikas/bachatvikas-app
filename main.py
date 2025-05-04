a1f145c279d700f57051909f13eabc440d2c0e20
Bachatvikas <info@bachatvikas.in>
2025-05-04 11:44:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="BachatVikas - Financial News and AI Summaries", layout="wide")
+
+st.title("📰 BachatVikas - Financial News and AI Summaries")
+
+# Sample static news item
+st.subheader("Govt terminates services of IMF ED K V Subramanian 6 months ahead of tenure")
+st.caption("🕒 May 03, 2025 17:52")
+st.image("https://img.theweek.in/content/dam/week/news/sci-tech/images/2021/12/16/PTI12_15_2021_000276A.jpg", use_column_width=True)
